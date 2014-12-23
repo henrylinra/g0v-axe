3f05e31a64a64e76d033028205e8337d146d9dc0
林慶豐 <lf12580@lf12580mba.local>
2014-12-23 14:37:28 +0800

    Solved level 1.

--- level_1.groovy
+++ level_1.groovy
@@ -5,3 +5,21 @@
 def tagsoupParser = new org.ccil.cowan.tagsoup.Parser()
 def slurper = new XmlSlurper(tagsoupParser)
 def htmlParser = slurper.parse("http://axe-level-1.herokuapp.com/")
+
+def trList = htmlParser.body.table.tr
+def title = trList[0]
+def result = []
+trList[1..-1].each { trItem ->
+	def row = [:] 
+    row["name"] = trItem.td[0]
+
+   	def grades = [:]
+    (1..5).each { i ->
+    	grades[title.td[i]]  = trItem.td[i]
+    }
+    row["grades"] = grades
+
+    result << row
+}
+
+println result
